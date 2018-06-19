30b00347e4745803b4d99cd9eeadcd89ce114b49
Matthew Weier O'Phinney <matthew@zend.com>
2018-06-19 17:31:22 -0500

    Use strict_types everywhere.

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -5,6 +5,8 @@
  * @license   https://github.com/zendframework/zend-datavalidator/blob/master/LICENSE.md New BSD License
  */
 
+declare(strict_types=1);
+
 namespace Zend\DataValidator\Exception;
 
 /**
