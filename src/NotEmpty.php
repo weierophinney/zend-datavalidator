9901e779459aec7b08d8fcc45f7cf08b2097fb3a
Matthew Weier O'Phinney <matthew@zend.com>
2018-06-20 10:23:06 -0500

    Refactors NotEmpty validator
    - Strict types.
    - Imports all referenced interfaces and functions.
    - Adds visibility to all constants.
    - Makes non-inherited instance variables private.
    - Splits `calculateTypeValue()` into `calculateTypeMask()` and
      `calculateTypeMaskFromArray()`, and now calls the former from within
      the constructor.
    - Renames `$defaultType` to `$defaultTypeMask`.
    - Adds a constructor for providing the type mask to validate against;
      this value is passed to `calculateTypeMask()`.
    - Splits all object-related conditionals to a separate
      `validateObjectValue()` method
    - Splits all string-related conditionals to a separate
      `validateStringValue()` method
    - Makes all conditionals compound, checking for both the type mask as
      well as strict equality to the known empty value the mask represents.

--- src/NotEmpty.php
+++ src/NotEmpty.php
@@ -1,32 +1,56 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-datavalidator for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-datavalidator/blob/master/LICENSE.md New BSD License
  */
 
+declare(strict_types=1);
+
 namespace Zend\DataValidator;
+
+use Countable;
+
+use function array_search;
+use function count;
+use function in_array;
+use function is_array;
+use function is_bool;
+use function is_float;
+use function is_int;
+use function is_object;
+use function is_string;
+use function method_exists;
+use function preg_match;
 
 final class NotEmpty extends AbstractValidator
 {
-    const BOOLEAN       = 0b000000000001;
-    const INTEGER       = 0b000000000010;
-    const FLOAT         = 0b000000000100;
-    const STRING        = 0b000000001000;
-    const ZERO          = 0b000000010000;
-    const EMPTY_ARRAY   = 0b000000100000;
-    const NULL          = 0b000001000000;
-    const PHP           = 0b000001111111;
-    const SPACE         = 0b000010000000;
-    const OBJECT        = 0b000100000000;
-    const OBJECT_STRING = 0b001000000000;
-    const OBJECT_COUNT  = 0b010000000000;
-    const ALL           = 0b011111111111;
+    public const BOOLEAN       = 0b000000000001;
+    public const INTEGER       = 0b000000000010;
+    public const FLOAT         = 0b000000000100;
+    public const STRING        = 0b000000001000;
+    public const ZERO          = 0b000000010000;
+    public const EMPTY_ARRAY   = 0b000000100000;
+    public const NULL          = 0b000001000000;
+    public const PHP           = 0b000001111111;
+    public const SPACE         = 0b000010000000;
+    public const OBJECT        = 0b000100000000;
+    public const OBJECT_STRING = 0b001000000000;
+    public const OBJECT_COUNT  = 0b010000000000;
+    public const ALL           = 0b011111111111;
 
-    const INVALID  = 'notEmptyInvalid';
-    const IS_EMPTY = 'isEmpty';
+    public const INVALID  = 'notEmptyInvalid';
+    public const IS_EMPTY = 'isEmpty';
 
-    protected $constants = [
+    /**
+     * @var array
+     */
+    protected $messageTemplates = [
+        self::IS_EMPTY => 'Value is required and can\'t be empty',
+        self::INVALID  => 'Invalid type given. String, integer, float, boolean or array expected',
+    ];
+
+    private $constants = [
         self::BOOLEAN       => 'boolean',
         self::INTEGER       => 'integer',
         self::FLOAT         => 'float',
@@ -43,11 +67,11 @@ final class NotEmpty extends AbstractValidator
     ];
 
     /**
-     * Default value for types; value = 0b000111101001
+     * Default types allowed; value = 0b000111101001
      *
      * @var array
      */
-    protected $defaultType = [
+    private $defaultTypeMask = [
         self::OBJECT,
         self::SPACE,
         self::NULL,
@@ -57,136 +81,181 @@ final class NotEmpty extends AbstractValidator
     ];
 
     /**
-     * @var array
+     * @var int Type mask of allowed types that can represent empty values.
      */
-    protected $messageTemplates = [
-        self::IS_EMPTY => 'Value is required and can\'t be empty',
-        self::INVALID  => 'Invalid type given. String, integer, float, boolean or array expected',
-    ];
+    private $typeMask;
 
     /**
-     * @param array|int|string $type
-     * @return int
+     * @param null|array|int|string $typeMask Allowed type(s) for representing
+     *     not-empty values. Defaults to the mask OBJECT|SPACE|NULL|EMPTY_ARRAY|STRING|BOOLEAN.
+     * @throws Exception\InvalidArgumentException if $typeMask is not null, an
+     *     integer, a string, or an array.
      */
-    protected function calculateTypeValue($type): int
+    public function __construct($typeMask = null)
     {
-        if (\is_array($type)) {
-            $detected = 0;
-            foreach ($type as $value) {
-                if (\is_int($value)) {
-                    $detected |= $value;
-                } elseif (\in_array($value, $this->constants, true)) {
-                    $detected |= \array_search($value, $this->constants, true);
-                }
-            }
-
-            $type = $detected;
-        } elseif (\is_string($type) && \in_array($type, $this->constants, true)) {
-            $type = \array_search($type, $this->constants, true);
+        if (null !== $typeMask
+            && ! (is_int($typeMask) || is_string($typeMask) || is_array($typeMask))
+        ) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                '$typeMask argument to %s MUST be an integer corresponding to one of'
+                . ' the type constants (or a mask of them), a string representation'
+                . ' of one of those constants, or an array of such values; received %s',
+                __CLASS__,
+                is_object($typeMask) ? get_class($typeMask) : gettype($typeMask)
+            ));
         }
 
-        return $type;
+        $this->typeMask = $this->calculateTypeMask($typeMask ?: $this->defaultTypeMask);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function validate($value, $context = null): ResultInterface
+    public function validate($value, $context = null) : ResultInterface
     {
-        if ($value !== null && ! \is_string($value) && ! \is_int($value) && ! \is_float($value) &&
-            ! \is_bool($value) && ! \is_array($value) && ! \is_object($value)
+        if ($value !== null
+            && ! is_string($value)
+            && ! is_int($value)
+            && ! is_float($value)
+            && ! is_bool($value)
+            && ! is_array($value)
+            && ! is_object($value)
         ) {
             return $this->createInvalidResult($value, [self::INVALID]);
         }
 
-        $type   = $this->calculateTypeValue($value);
-        $object = false;
-
-        // OBJECT_COUNT (countable object)
-        if ($type & self::OBJECT_COUNT) {
-            $object = true;
-
-            if (\is_object($value) && ($value instanceof \Countable) && (\count($value) === 0)) {
-                return $this->createInvalidResult($value,[self::IS_EMPTY]);
-            }
-        }
-
-        // OBJECT_STRING (object's toString)
-        if ($type & self::OBJECT_STRING) {
-            $object = true;
-
-            if ((\is_object($value) && (! method_exists($value, '__toString'))) ||
-                (\is_object($value) && method_exists($value, '__toString') && (((string) $value) === ''))) {
-                return $this->createInvalidResult($value, [self::IS_EMPTY]);
-            }
-        }
-
-        // OBJECT (object)
-        if ($type & self::OBJECT) {
-            // fall trough, objects are always not empty
-        } elseif ($object === false) {
-            // object not allowed but object given -> return false
-            if (\is_object($value)) {
-                return $this->createInvalidResult($value, [self::IS_EMPTY]);
-            }
-        }
-
-        // SPACE ('   ')
-        if ($type & self::SPACE) {
-            if (\is_string($value) && preg_match('/^\s+$/s', $value)) {
-                return $this->createInvalidResult($value, [self::IS_EMPTY]);
-            }
-        }
-
         // NULL (null)
-        if ($type & self::NULL) {
-            if ($value === null) {
-                return $this->createInvalidResult($value, [self::IS_EMPTY]);
-            }
-        }
-
-        // EMPTY_ARRAY (array())
-        if ($type & self::EMPTY_ARRAY) {
-            if (\is_array($value) && ($value === [])) {
-                return $this->createInvalidResult($value, [self::IS_EMPTY]);
-            }
-        }
-
-        // ZERO ('0')
-        if ($type & self::ZERO) {
-            if (\is_string($value) && ($value === '0')) {
-                return $this->createInvalidResult($value, [self::IS_EMPTY]);
-            }
-        }
-
-        // STRING ('')
-        if ($type & self::STRING) {
-            if (\is_string($value) && ($value === '')) {
-                return $this->createInvalidResult($value, [self::IS_EMPTY]);
-            }
-        }
-
-        // FLOAT (0.0)
-        if ($type & self::FLOAT) {
-            if (\is_float($value) && ($value === 0.0)) {
-                return $this->createInvalidResult($value, [self::IS_EMPTY]);
-            }
-        }
-
-        // INTEGER (0)
-        if ($type & self::INTEGER) {
-            if (\is_int($value) && ($value === 0)) {
-                return $this->createInvalidResult($value, [self::IS_EMPTY]);
-            }
+        if ($this->typeMask & self::NULL
+            && null === $value
+        ) {
+            return $this->createInvalidResult($value, [self::IS_EMPTY]);
         }
 
         // BOOLEAN (false)
-        if ($type & self::BOOLEAN) {
-            if (\is_bool($value) && ($value === false)) {
-                return $this->createInvalidResult($value, [self::IS_EMPTY]);
-            }
+        if ($this->typeMask & self::BOOLEAN
+            && $value === false
+        ) {
+            return $this->createInvalidResult($value, [self::IS_EMPTY]);
         }
 
-        return new Result(true, $value);
+        // EMPTY_ARRAY ([])
+        if ($this->typeMask & self::EMPTY_ARRAY
+            && $value === []
+        ) {
+            return $this->createInvalidResult($value, [self::IS_EMPTY]);
+        }
+
+        // FLOAT (0.0)
+        if ($this->typeMask & self::FLOAT
+            && $value === 0.0
+        ) {
+            return $this->createInvalidResult($value, [self::IS_EMPTY]);
+        }
+
+        // INTEGER (0)
+        if ($this->typeMask & self::INTEGER
+            && $value === 0
+        ) {
+            return $this->createInvalidResult($value, [self::IS_EMPTY]);
+        }
+
+        // SPACE|ZERO|STRING (' ', '0', '')
+        if (is_string($value)) {
+            return $this->validateStringValue($value);
+        }
+
+        // OBJECT|OBJECT_STRING|OBJECT_INT
+        if (is_object($value)) {
+            return $this->validateObjectValue($value);
+        }
+
+        return Result::createValidResult($value);
+    }
+
+    /**
+     * @param array|int|string $typeMask
+     */
+    private function calculateTypeMask($typeMask) : int
+    {
+        if (is_array($typeMask)) {
+            return $this->calculateTypeMaskFromArray($typeMask);
+        }
+
+        if (is_string($typeMask) && in_array($typeMask, $this->constants, true)) {
+            return array_search($typeMask, $this->constants, true);
+        }
+
+        return $typeMask;
+    }
+
+    private function calculateTypeMaskFromArray(array $types) : int
+    {
+        $typeMask = 0;
+        foreach ($types as $type) {
+            if (is_int($type)) {
+                $typeMask |= $type;
+                continue;
+            }
+
+            if (in_array($type, $this->constants, true)) {
+                $typeMask |= array_search($type, $this->constants, true);
+                continue;
+            }
+        }
+        return $typeMask;
+    }
+
+    /**
+     * @param object $value
+     */
+    private function validateObjectValue($value) : ResultInterface
+    {
+        // Object-as-integer, but counting results in zero.
+        if ($this->typeMask & self::OBJECT_COUNT
+            && $value instanceof Countable
+            && count($value) === 0
+        ) {
+            return $this->createInvalidResult($value, [self::IS_EMPTY]);
+        }
+
+        // Object-as-string, but results in empty string.
+        if ($this->typeMask & self::OBJECT_STRING
+            && (! method_exists($value, '__toString') || (string) $value === '')
+        ) {
+            return $this->createInvalidResult($value, [self::IS_EMPTY]);
+        }
+
+        // Objects (including countable and string represenatations) are
+        // allowed; objects are never empty, so valid
+        $objectMask = self::OBJECT | self::OBJECT_COUNT | self::OBJECT_STRING;
+        if ($this->typeMask & $objectMask) {
+            return Result::createValidResult($value);
+        }
+
+        // Objects are not allowed, but we have one.
+        return $this->createInvalidResult($value, [self::IS_EMPTY]);
+    }
+
+    private function validateStringValue(string $value) : ResultInterface
+    {
+        if ($this->typeMask & self::SPACE
+            && preg_match('/^\s+$/s', $value)
+        ) {
+            return $this->createInvalidResult($value, [self::IS_EMPTY]);
+        }
+
+        if ($this->typeMask & self::ZERO
+            && $value === '0'
+        ) {
+            return $this->createInvalidResult($value, [self::IS_EMPTY]);
+        }
+
+        if ($this->typeMask & self::STRING
+            && $value === ''
+        ) {
+            return $this->createInvalidResult($value, [self::IS_EMPTY]);
+        }
+
+        return Result::createValidResult($value);
     }
 }
